f439bf6148043cec76abbd87c99e5b7451d3b10e
Muhammad Yamin <muhammad.yamin@devcrew.io>
2024-02-21 10:57:04 +0500

    style changes - 1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -19,15 +19,9 @@ const goToImageCredits = () => {
       </div>
     <p>A small blurb about the AR experience, highlighting the centre’s hopes to inform and preserve the history of Birkenhead Point</p>
     <p>Birkenhead Point continues to be a vibrant centre for our customers. As Sydney's best-loved premium brand outlet with more than 160 local and international brands, water-front dining and exciting entertainment our customers can expect a day out nothing short of memorable when visiting. Discover what’s currently on at Birkenhead Point here:</p>
-    <a href="https://www.birkenheadpoint.com.au/">https://www.birkenheadpoint.com.au/</a>
+    <p><a href="https://www.birkenheadpoint.com.au/">https://www.birkenheadpoint.com.au/</a></p>
+    <p><a class="cta-image-credits" @click="goToImageCredits">Image Credits</a></p>
   </div>
-    <div class="d-flex justify-content-center"  @click="goToImageCredits">
-      <a class="cta-image-credits">Image Credits
-        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="12" viewBox="0 0 8 12" fill="none">
-          <path d="M1.50004 1C1.50004 1 6.49999 4.68244 6.5 6.00004C6.50001 7.31763 1.5 11 1.5 11" stroke="#141B34" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
-      </a>
-    </div>
 	    <div class="text-center">
 			<button @click="logout" class="btn btn-primary logout">Logout</button>
 	    </div>
